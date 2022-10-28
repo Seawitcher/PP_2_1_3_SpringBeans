19a761dd557cf5580b232358562179f2d7e05a3c
Seawitcher <Seawitcher83@yandex.ru>
2022-10-28 15:00:37 +0400

    Version 0.1

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,19 @@
 package app.config;
 
+import app.model.Animal;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean
+    public Animal dogBean(){
+    Animal dog = new Dog();
+        return dog;
+    }
 }
